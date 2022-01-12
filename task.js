3160c13534f63a85b4ae02b8887dea393a2451b3
Eliza <eliza.maria.nowakowska@wp.pl>
2022-01-12 17:48:45 +0100

    completed tasks

--- task.js
+++ task.js
@@ -5,7 +5,30 @@ Import array with users (from user1.json file) and write a function that will ch
 Result:
 - if the phone number contains only digits then log true in the console
 - if the phone number does not contain only digits then log which user's phone number is not correct e.g. "Phone number of user with ID 2 is not correct"
+*/
 
+
+console.log('\nTask 1\n------\n');
+
+let fs = require('fs');
+let json = fs.readFileSync('users1.json');
+let users1 = JSON.parse(json);
+
+function numberCheck(users) {
+    for(let i in users) {
+        if(users[i].phoneNumber.match(/^[0-9]+$/) != null) {
+            console.log('Phone number of User with userId ' + users[i].userId + ': true');
+        }
+        else {
+            console.log('Phone number of User with userId ' + users[i].userId + ': FALSE');
+        }
+        i++;
+    }
+}
+numberCheck(users1.users);
+
+
+/*
 Task 2
 Import array with users (from user1.json file) and users (from user2.js file) and compare them
 Result:
@@ -14,3 +37,29 @@ Result:
 
 Compare email addresses, first names, last names and phone numbers.
 */
+
+
+//I'm not going to import users from users1.json again and just use the import from Task 1
+
+console.log('\n\nTask 2\n------');
+
+const users2 = require('./users2.js');
+
+function compareUsers(users1, users2) {
+    let howManyFields;
+    let compareField;
+    for(let i in users1) {
+        howManyFields = Object.keys(users1[i]).length;
+        for(let j = 1; j < howManyFields; j++) {
+            compareField = Object.keys(users1[i])[j];
+            if(users1[i][compareField] != users2[i][compareField]) {
+                console.log('\nDifferent values between users with Id: ' + users1[i].userId + ' on field: ' + compareField);
+                console.log('1st ' + compareField + ': ' + users1[i][compareField]);
+                console.log('2nd ' + compareField + ': ' + users2[i][compareField]);
+            }
+        }
+        i++;
+    }
+}
+
+compareUsers(users1.users, users2.users);
